2d69428b53929298896eba97677d4c62c18be17f
m-this <52073029+m-this@users.noreply.github.com>
2026-07-12 08:51:05 +0200

    feat(install-pkg): add --skip-extract option
    - port v2 install-pkg --skip-extract: download the package artifact only
    - downloads via ArtifactDownloader with binary preference, skipping the installer

--- src/StaticPHP/Command/InstallPackageCommand.php
+++ src/StaticPHP/Command/InstallPackageCommand.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace StaticPHP\Command;
 
+use StaticPHP\Artifact\ArtifactDownloader;
 use StaticPHP\Artifact\DownloaderOptions;
 use StaticPHP\DI\ApplicationContext;
+use StaticPHP\Exception\WrongUsageException;
 use StaticPHP\Package\PackageInstaller;
 use StaticPHP\Registry\PackageLoader;
 use Symfony\Component\Console\Attribute\AsCommand;
@@ -30,12 +32,25 @@ class InstallPackageCommand extends BaseCommand
                 return array_filter($packages, fn ($name) => str_starts_with($name, $val));
             }
         );
+        $this->addOption('skip-extract', null, null, 'Skip package extraction, just download the package archive');
         $this->getDefinition()->addOptions(DownloaderOptions::getConsoleOptions('dl'));
     }
 
     public function handle(): int
     {
         ApplicationContext::set('elephant', true);
+        if ($this->getOption('skip-extract')) {
+            $package = PackageLoader::getPackage($this->input->getArgument('package'));
+            if (($artifact = $package->getArtifact()) === null) {
+                throw new WrongUsageException("Package '{$package->getName()}' does not have an artifact to download.");
+            }
+            $downloader = new ArtifactDownloader([
+                ...DownloaderOptions::extractFromConsoleOptions($this->input->getOptions(), 'dl'),
+                'prefer-binary' => true,
+            ]);
+            $downloader->add($artifact)->download();
+            return static::SUCCESS;
+        }
         $installer = new PackageInstaller([...$this->input->getOptions(), 'dl-prefer-binary' => true], true);
         $installer->addInstallPackage($this->input->getArgument('package'));
         $installer->run(true);
