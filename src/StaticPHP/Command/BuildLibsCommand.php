208a27ac44436d65542a67155cd468064fedab77
m-this <52073029+m-this@users.noreply.github.com>
2026-07-12 08:50:33 +0200

    feat(build): add clean action to build:libs command
    - add --clean option to build:libs, ported from v2 BuildLibsCommand
    - removes buildroot/ before building, following the download --clean style

--- src/StaticPHP/Command/BuildLibsCommand.php
+++ src/StaticPHP/Command/BuildLibsCommand.php
@@ -7,6 +7,8 @@ namespace StaticPHP\Command;
 use StaticPHP\Artifact\DownloaderOptions;
 use StaticPHP\Package\PackageInstaller;
 use StaticPHP\Registry\PackageLoader;
+use StaticPHP\Util\FileSystem;
+use StaticPHP\Util\InteractiveTerm;
 use StaticPHP\Util\V2CompatLayer;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Completion\CompletionInput;
@@ -33,6 +35,7 @@ class BuildLibsCommand extends BaseCommand
         );
         // Builder options
         $this->getDefinition()->addOptions([
+            new InputOption('clean', null, null, 'Clean old buildroot dir before build'),
             new InputOption('with-suggests', ['L', 'E'], null, 'Resolve and install suggested packages as well'),
             new InputOption('with-packages', null, InputOption::VALUE_REQUIRED, 'add additional packages to install/build, comma separated', ''),
             new InputOption('no-download', null, null, 'Skip downloading artifacts (use existing cached files)'),
@@ -45,6 +48,19 @@ class BuildLibsCommand extends BaseCommand
     public function handle(): int
     {
         $this->checkDoctorCache();
+
+        // handle --clean option
+        if ($this->getOption('clean')) {
+            logger()->warning('You are doing some operations that are not recoverable:');
+            logger()->warning('- Removing directory: ' . BUILD_ROOT_PATH);
+            logger()->alert('I will remove this directory after 5 seconds!');
+            sleep(5);
+            if (is_dir(BUILD_ROOT_PATH)) {
+                InteractiveTerm::indicateProgress('Removing: ' . BUILD_ROOT_PATH);
+                FileSystem::removeDir(BUILD_ROOT_PATH);
+                InteractiveTerm::finish('Removed: ' . BUILD_ROOT_PATH);
+            }
+        }
 
         $libs = parse_comma_list($this->input->getArgument('libraries'));
 
